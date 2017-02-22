cc059198acafbd3e96b5289a95c2691b63eb815d
crashko <martin_kovac_svk@gmail.com>
2017-02-22 16:08:59 +0100

    first commit

---
+++ src/main/java/com/capco/communicator/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.capco.communicator;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+	List<Customer> findByLastNameStartsWithIgnoreCase(String lastName);
+}
